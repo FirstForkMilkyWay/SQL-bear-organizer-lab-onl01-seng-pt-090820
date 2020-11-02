67f925720bef5fe06c5c4636923093aa0de142b9
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-11-02 21:51:08 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,5 +4,5 @@ INSERT INTO bears (name) VALUES ("Mr. Chocolate");
 INSERT INTO bears (name) VALUES ("Rowdy");
 INSERT INTO bears (name) VALUES ("Mr. Chocolate");
 INSERT INTO bears (name) VALUES ("Rowdy");
-INSERT INTO bears (name) VALUES ("Mr. Chocolate");
+INSERT INTO bears (name) VALUES ("Wendy");
 INSERT INTO bears (name) VALUES ("NULL");
